250fb7dc0c1cf4e15cf21f7b88436842ef4d80d8
Emma Fewer <emmafewer20@gmail.com>
2020-11-18 16:35:38 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,7 @@
+#perform a constant exprssion evaluation
+3
+#assign constant 3 to bare-word variable triangle_sides
+triangle_sides = 3
+puts "Joy to the world"
+puts "all the boys and girls"
+#puts "Joy to the fishes and the deep blue sea"
